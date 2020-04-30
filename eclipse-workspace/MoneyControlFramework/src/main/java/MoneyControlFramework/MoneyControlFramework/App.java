9c9e4b884241b255de434675f6d9e2ebf21a552e
shrijayant <shrijayant.sjj@gmail.com>
2020-05-01 00:25:29 +0530

    first commit

---
+++ eclipse-workspace/MoneyControlFramework/src/main/java/MoneyControlFramework/MoneyControlFramework/App.java
@@ -0,0 +1,13 @@
+package MoneyControlFramework.MoneyControlFramework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
